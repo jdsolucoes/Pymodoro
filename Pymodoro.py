8334f039b877796c491dc2d8f4ce83643c074e45
Jefferson Daniel <jd.solucoes@gmail.com>
2015-06-04 05:35:07 -0300

    Some pep8 5 years later :)

--- Pymodoro.py
+++ Pymodoro.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 #   This file is part of Pymodoro.
 
 #   Pymodoro is free software: you can redistribute it and/or modify
@@ -14,32 +14,37 @@
 #    You should have received a copy of the GNU General Public License
 #    along with Pymodoro.  If not, see <http://www.gnu.org/licenses/>.
 
-import time, os, gobject
-import pygtk, pango
+import time
+import os
+import gobject
+import pygtk
+import pango
 import datetime
 
 from modules.db import db
 from modules.playback import play
 
+import gtk
+from gtk import glade
+pygtk.require('2.0')
+
+
 try:
     import pynotify
     notify = True
-except:
+except ImportError:
     print "Pynotify was not found, disabling it."
     notify = None
 
-pygtk.require('2.0')
-import gtk
-from gtk import glade
 
-class Pymodoro:
+class Pymodoro(object):
 
     alarms = {
-        '2minutes' : 'files/sound/2minuteswarning.wav',
-        'inicio' : 'files/sound/inicio.wav',
-        'alarm' : 'files/sound/alarm.wav',
-        'pausacurta' : 'files/sound/5minutos.wav',
-        'pausalonga' : 'files/sound/pausalonga.wav'
+        '2minutes': 'files/sound/2minuteswarning.wav',
+        'inicio': 'files/sound/inicio.wav',
+        'alarm': 'files/sound/alarm.wav',
+        'pausacurta': 'files/sound/5minutos.wav',
+        'pausalonga': 'files/sound/pausalonga.wav'
     }
     time_seconds = 1501
     half_break_time = 301
@@ -61,7 +66,7 @@ class Pymodoro:
         self.label_clock.set_padding(10, 5)
         self.main_window.show()
 
-        #the list itself.
+        # The list itself.
         self.list_tasks = gtk.ListStore(int, int, str, int, str)
         self.tree_tasks = self.w_gui.get_widget('listaTarefas')
         self.tree_tasks.set_model(self.list_tasks)
@@ -75,28 +80,28 @@ class Pymodoro:
         task_column.set_attributes(cell, text=2, strikethrough=1)
         self.tree_tasks.append_column(task_column)
 
-        #systray icon
+        # Systray icon
         self.staticon = gtk.StatusIcon()
         self.staticon.connect("button_press_event", self.activate)
         self.staticon.set_from_file("files/img/pomodoro.png")
         self.staticon.set_tooltip("Pymodoro - Clique para Mostrar/Esconder")
         self.staticon.set_visible(True)
 
-        #call populate_task_list()
+        # Call populate_task_list()
         self.populate_task_list()
 
-        #connecting the signals from GLADE/GTK to functions in python.
+        # Connecting the signals from GLADE/GTK to functions in python.
         signals = {
-            'on_listaTarefas_cursor_changed' : self.update_info,
-            'on_esconderMenu_activate' : self.hide_done_tasks,
-            'on_finalizarMenu_activate' : self.finish_task,
-            'on_deleteMenu_activate' : self.delete_task,
-            'on_imagemenuitem5_activate' : gtk.main_quit,
-            'on_janelaPrincipal_destroy' : gtk.main_quit,
-            'on_startBotao_clicked' : self.start_timer,
-            'on_stopBotao_clicked' : self.stop_timer,
-            'on_calendario_day_selected' : self.populate_task_list,
-            'on_addBotao_clicked' : self.add_tarefa
+            'on_listaTarefas_cursor_changed': self.update_info,
+            'on_esconderMenu_activate': self.hide_done_tasks,
+            'on_finalizarMenu_activate': self.finish_task,
+            'on_deleteMenu_activate': self.delete_task,
+            'on_imagemenuitem5_activate': gtk.main_quit,
+            'on_janelaPrincipal_destroy': gtk.main_quit,
+            'on_startBotao_clicked': self.start_timer,
+            'on_stopBotao_clicked': self.stop_timer,
+            'on_calendario_day_selected': self.populate_task_list,
+            'on_addBotao_clicked': self.add_tarefa
         }
         self.w_gui.signal_autoconnect(signals)
 
@@ -120,7 +125,7 @@ class Pymodoro:
         """
         model, iter = self.tree_tasks.get_selection().get_selected()
         if model and iter:
-            id = model.get_value(iter,0)
+            id = model.get_value(iter, 0)
             db().removeTask(id)
             self.populate_task_list()
 
@@ -128,29 +133,29 @@ class Pymodoro:
         else:
             return None
 
-    def finish_task(self,obj=None):
+    def finish_task(self, obj=None):
         """
         Set the current task as done/todo
         """
-        model, iter = self.tree_tasks.get_selection().get_selected()
-        if model and iter:
-            id = model.get_value(iter,0)
-            fineshed = model.get_value(iter,1)
+        model, iterator = self.tree_tasks.get_selection().get_selected()
+        if model and iterator:
+            i = model.get_value(iterator, 0)
+            fineshed = model.get_value(iterator, 1)
             if fineshed == 1:
-                db().update('tarefas',id,concluido=0)
+                db().update('tarefas', i, concluido=0)
             else:
-                db().update('tarefas',id,concluido=1)
+                db().update('tarefas', i, concluido=1)
             self.populate_task_list()
 
-    def menu_popup(self,obj, event):
+    def menu_popup(self, obj, event):
         """
         menu_popup, shows the menu for the treeview
         """
-        #if the mouse button isn't the Right button...pass
+        # If the mouse button isn't the Right button...pass
         if event.button != 3:
             pass
         else:
-            path = obj.get_path_at_pos(int(event.x),int(event.y))
+            path = obj.get_path_at_pos(int(event.x), int(event.y))
             selection = obj.get_selection()
             selected = selection.get_selected_rows()
             if not path:
@@ -162,21 +167,19 @@ class Pymodoro:
                 pass
             else:
                 model, iter = selection.get_selected()
-                #get the current status of the task(done or to do);
-                done = model.get_value(iter,1)
+                # Get the current status of the task(done or to do);
+                done = model.get_value(iter, 1)
                 m = gtk.Menu()
                 r_task = gtk.MenuItem("Remover Tarefa")
-                #if done is equal to one it means that the task is done
+                # If done is equal to one it means that the task is done
                 if done == 1:
-                    #show this then
                     m_done = gtk.MenuItem("Desmarcar como feita")
                 else:
-                    #if not show this
                     m_done = gtk.MenuItem("Marcar como feita")
 
             # Connecting the events.
-            m_done.connect('activate',self.finish_task)
-            r_task.connect('activate',self.delete_task)
+            m_done.connect('activate', self.finish_task)
+            r_task.connect('activate', self.delete_task)
             # Showing all!
             r_task.show()
             m_done.show()
@@ -194,7 +197,7 @@ class Pymodoro:
         calendario = self.w_gui.get_widget('calendario')
         calendario.clear_marks()
         day, month, year = self.get_calendar()
-        dias = db().getByDate(None,month)
+        dias = db().getByDate(None, month)
         for i in dias:
             calendario.mark_day(i[4].day)
 
@@ -204,15 +207,15 @@ class Pymodoro:
         month += 1
         day = "%02d" % day
 
-        return day,month,year
+        return day, month, year
 
     def populate_task_list(self, obj=None):
         """
         Populate the taks list
         and then calls self.mark_days()
         """
-        day,month,year = self.get_calendar()
-        tasks = db().getListOfTasks(day,month,year)
+        day, month, year = self.get_calendar()
+        tasks = db().getListOfTasks(day, month, year)
         # Remove all items on the list
         for i in self.list_tasks:
             self.list_tasks.remove(i.iter)
@@ -235,15 +238,15 @@ class Pymodoro:
                 self.startTime = time.time() + self.full_break_time
             else:
                 self.startTime = time.time() + self.half_break_time
-            self.thread_pid = gobject.timeout_add(10,self.run)
+            self.thread_pid = gobject.timeout_add(10, self.run)
         else:
             if self.thread_pid == 0 and model and iter:
                 self.alarm(self.alarms['inicio'])
                 self.lock_notify = None
-                self.idAtivo = model.get_value(iter,0)
+                self.idAtivo = model.get_value(iter, 0)
                 self.startTime = time.time() + self.time_seconds
-                self.thread_pid = gobject.timeout_add(10,self.run)
-                self.change_icon('Normal',True)
+                self.thread_pid = gobject.timeout_add(10, self.run)
+                self.change_icon('Normal', True)
 
     def activate(self, obj, event):
         """Stupid ass name, i know, shame on me"""
@@ -266,9 +269,9 @@ class Pymodoro:
         """
         model, iter = obj.get_selection().get_selected()
         if iter:
-            id = model.get_value(iter,0)
+            id = model.get_value(iter, 0)
             row = db().getByID(id)
-            data = datetime.datetime.strftime(row[4],'%d/%m/%Y')
+            data = datetime.datetime.strftime(row[4], '%d/%m/%Y')
             self.w_gui.get_widget('labelData').set_text(data)
             self.w_gui.get_widget('labelPomodoros').set_text(str(row[3]))
             status = row[1]
@@ -290,18 +293,18 @@ class Pymodoro:
             self.startTime = time.time()
             self.thread_pid = 0
 
-
-    def notificate(self,msg=None):
+    def notificate(self, msg=None):
         """Use pynotify to emit a notification, looks great in ubuntu"""
         if notify and msg:
             if pynotify.init('Pymodoro'):
-                n = pynotify.Notification('Pymodoro', msg, "%s/img/pomodoro.png" % self.filesDir)
+                n = pynotify.Notification(
+                    'Pymodoro', msg, "%s/img/pomodoro.png" % self.filesDir)
                 n.show()
                 return True
         else:
             return None
 
-    def change_icon(self,icon=None,blinking=None):
+    def change_icon(self, icon=None, blinking=None):
         """Use the change the status of the icon, blinking or not, Normal or
         break time"""
         if icon:
@@ -337,9 +340,9 @@ class Pymodoro:
         else:
             pomodoros = db().getPomodoros(self.idAtivo) + 1
             self.w_gui.get_widget('labelPomodoros').set_text(str(pomodoros))
-            db().update('tarefas',self.idAtivo,pomodoros=pomodoros)
+            db().update('tarefas', self.idAtivo, pomodoros=pomodoros)
             self.pomodoros = self.pomodoros + 1
-            self.change_icon('Break',True)
+            self.change_icon('Break', True)
             self.notificate('Iniciando pausa... 5 minutos, corre negada!.')
             self.alarm(self.alarms['pausacurta'])
             self.active_break = True
@@ -347,7 +350,7 @@ class Pymodoro:
 
     def hide_done_tasks(self, obj=None):
         """Hidden the tasks tagged as done"""
-        if self.show_done_tasks == False:
+        if not self.show_done_tasks:
             self.show_done_tasks = True
         else:
             self.show_done_tasks = False
@@ -355,16 +358,15 @@ class Pymodoro:
 
     def run(self):
         """the function that holds the clock itself"""
-        diff_time =  self.startTime - time.time()
+        diff_time = self.startTime - time.time()
         (minutes, seconds) = divmod(diff_time, 60.0)
         if int(diff_time) == 0:
             self.finished()
             return False
         else:
-            self.label_clock.set_text("%02i:%02i" % (minutes,seconds))
+            self.label_clock.set_text("%02i:%02i" % (minutes, seconds))
 
         if int(diff_time) < 120:
-
             if self.lock_notify:
                 pass
             else:
